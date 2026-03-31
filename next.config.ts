fd3fed996ab163dad104588eb37604eac74b427b
Mateo Leal <mateolegi@gmail.com>
2026-03-31 15:40:30 -0500

    feat: ensure minimumCacheTTL is set for image caching in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://wiki.warframe.com/images/**')],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
   },
 }
 
